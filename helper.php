54713b24bb745a7542d70ba1ba72df89b90c73c6
VAegorov <kot2046@gmail.com>
2018-01-20 21:16:17 +0300

    create function connect

--- helper.php
+++ helper.php
@@ -1,1 +1,12 @@
 <?php
+
+function connect()
+{
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db_name = 'test';
+
+    $link = mysqli_connect($host, $user, $password, $db_name);
+    return $link;
+}
